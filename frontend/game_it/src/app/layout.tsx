bc3be0ded934d9c040d07f314db319729890ba27
Milya <8milia01@mail.ru>
2024-11-16 12:25:56 +0700

    layout is ready

--- frontend/game_it/src/app/layout.tsx
+++ frontend/game_it/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "./header";
+import Footer from "./footer";
+import Providers from "./providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {/* <Providers> */}
+          <Header />
+          {children}
+          <Footer />
+        {/* </Providers> */}
       </body>
     </html>
   );
