d633117c7d669af6c8898bd1b39ed76601469722
Milya <8milia01@mail.ru>
2024-11-16 16:11:52 +0700

    xp

--- frontend/game_it/src/app/providers.tsx
+++ frontend/game_it/src/app/providers.tsx
@@ -16,11 +16,10 @@ const wagmiConfig = createConfig({
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-
-  console.log(process.env.NEXT_PUBLIC_PRIVY_APP_ID)
   return (
     <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      // appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      appId="cm3jxft0s06ulxecy1vc0mzqe" 
       config={{
         appearance: {
           theme: "light",
