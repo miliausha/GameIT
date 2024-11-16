2e0ed7db3bd318665776962dce1a9450c48d7d70
Valeria <70722334+Hikabu@users.noreply.github.com>
2024-11-16 23:16:10 +0700

    ai player for time

--- Game/app/game/scenes/RealPlay.ts
+++ Game/app/game/scenes/RealPlay.ts
@@ -1,33 +1,32 @@
-// scenes/RealPlay.ts
 import { Scene } from 'phaser';
 
 interface GameData {
     playerChoice: string;
     timeExpired: boolean;
 }
-
 export class RealPlay extends Scene {
     private playerChoice: string;
     private timeExpired: boolean;
-
+    private aiChoice: string;
+    private possibleChoices: string[] = ['rock', 'paper', 'scissors'];
     constructor() {
         super('RealPlay');
     }
-
     init(data: GameData) {
         this.playerChoice = data.playerChoice;
         this.timeExpired = data.timeExpired;
-    }
-
-    create() {
+        this.aiChoice = this.possibleChoices[Math.floor(Math.random() * this.possibleChoices.length)];
+    }create() {
         const background = this.add.image(512, 384, 'background');
         background.setAlpha(0.5);
 
         const choiceImage = this.add.image(150, 384, this.playerChoice)
             .setOrigin(0.5)
             .setScale(0.2);
-
-
+        const aiChoiceImage = this.add.image(874, 384, this.aiChoice)
+            .setOrigin(0.5)
+            .setScale(0.2)
+            .setFlipX(true);
         const buttonStyle = {
             fontFamily: 'Arial Black',
             fontSize: 32,
@@ -35,11 +34,8 @@ export class RealPlay extends Scene {
             backgroundColor: '#000000',
             padding: { x: 20, y: 10 },
             fixedWidth: 200
-        };
-
-        const menuButton = this.add.text(512, 500, 'MENU', buttonStyle)
+        };const menuButton = this.add.text(512, 500, 'MENU', buttonStyle)
             .setOrigin(0.5)
             .setInteractive()
             .on('pointerdown', () => this.scene.start('MainMenu'));
-    }
-}
+    }}
