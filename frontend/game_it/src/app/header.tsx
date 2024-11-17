ff96ccb5fde3fa0a6a3111fe5073f9d40983749c
Milya <8milia01@mail.ru>
2024-11-17 08:23:29 +0700

    last commit

--- frontend/game_it/src/app/header.tsx
+++ frontend/game_it/src/app/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     const { wallets } = useWallets();
     const [isHovering, setIsHovering] = useState(false)
 
-    const shortenAddress = (address: sgittring) => {
+    const shortenAddress = (address: string) => {
         return `${address.slice(0, 6)}...${address.slice(-4)}`;
     };
     return (
